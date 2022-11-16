95e9dcff647220c7b94e332bd74770253bc2af8a
Daniel Stotts <dstodev@gmail.com>
2022-11-15 20:50:07 -0800

    Cleanup

--- udp_listener.py
+++ udp_listener.py
@@ -9,7 +9,7 @@ def main():
 	while True:
 		# https://docs.python.org/3/library/socket.html#socket.socket.recvfrom
 		bytes, address = sock.recvfrom(128)
-		timestamp = datetime.now().strftime("%I:%M:%S%p")  # Record time as soon as possible after it was received
+		timestamp = datetime.now().strftime("%I:%M:%S %p")  # Record time as soon as possible after it was received
 		message = bytes.decode('ascii')
 		ip = address[0]
 		port = address[1]
