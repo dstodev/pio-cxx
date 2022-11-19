866ccfbcbbd2f10aa86c58a0d770a81d740b89c4
Daniel Stotts <dstodev@gmail.com>
2022-11-18 21:16:30 -0800

    style

--- lib/my/src/constants.hxx
+++ lib/my/src/constants.hxx
@@ -2,10 +2,10 @@
 #define PIO_CXX_CONSTANTS_HXX
 
 constexpr auto UdpBroadcastPort = 58'400;
-constexpr auto LoopDelay = 5'000 /*ms*/;
-constexpr auto WaitForSerialDelay = 10'000 /*ms*/;
-constexpr auto WaitForWiFiDelay = 10'000 /*ms*/;
-constexpr auto WatchdogPanicTimeout = 20 /*seconds*/;
-constexpr auto SerialBaudRate = 112'500 /*bits per second*/;
+constexpr auto LoopDelay = 5'000 /* ms */;
+constexpr auto WaitForSerialDelay = 10'000 /* ms */;
+constexpr auto WaitForWiFiDelay = 10'000 /* ms */;
+constexpr auto WatchdogPanicTimeout = 20 /* seconds */;
+constexpr auto SerialBaudRate = 112'500 /* bits per second */;
 
 #endif  // PIO_CXX_CONSTANTS_HXX
--- lib/my/src/wait_for.hxx
+++ lib/my/src/wait_for.hxx
@@ -7,7 +7,7 @@ namespace my {
 
 /// Wait up to timeout_ms for predicate, checking every interval_ms.
 /// A predicate is anything testable as a boolean, especially stateful classes which implement operator bool().
-/// Requires a WaitFn(/*uint32_t ms*/) to call, which is expected to wait for the given time in milliseconds.
+/// Requires a WaitFn(/* uint32_t ms */) to call, which is expected to wait for the given time in milliseconds.
 template <typename Predicate, typename WaitFn>
 void wait_for(Predicate& predicate, WaitFn& tick_fn, uint32_t timeout_ms, uint32_t interval_ms = 10 /*ms*/)
 {
