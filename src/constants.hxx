40ab1ebfb2c68cd0e401b53e760f5c12810b8bb4
Daniel Stotts <dstodev@gmail.com>
2022-11-27 01:47:13 -0800

    Waits longer for connection

--- src/constants.hxx
+++ src/constants.hxx
@@ -7,7 +7,7 @@ static_assert(SleepWakeupDelay >= 1, "Less than a second might not be worth slee
 constexpr auto SerialBaudRate = 112'500 /* bits per second */;
 constexpr auto UdpBroadcastPort = 58'400;
 constexpr auto WaitForSerialDelay = 5 /* seconds */;
-constexpr auto WaitForWiFiDelay = 5 /* seconds */;
+constexpr auto WaitForWiFiDelay = 10 /* seconds */;
 
 constexpr auto WatchdogPanicTimeout = SleepWakeupDelay * 2;
 static_assert(WatchdogPanicTimeout > SleepWakeupDelay, "WatchdogPanicTimeout must exceed LoopCycleTime.");
